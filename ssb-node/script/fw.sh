c4bafb865f26c322940f9437bbfd6ad86acf5f8e
Fredrik Engberg <fredrik.w.engberg@gmail.com>
2019-02-15 16:11:47 +0100

    - Cleanup fw-scripts.

--- ssb-node/script/fw.sh
+++ ssb-node/script/fw.sh
@@ -1,11 +1,8 @@
 #!/bin/ash
-
+# Configure iptables to only accept related packets, no incoming connections allowed.
 iptables -F
 iptables -A INPUT -i lo -j ACCEPT
-
 iptables -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-
 iptables -P OUTPUT ACCEPT
 iptables -P INPUT DROP
-
 iptables -L -vn
